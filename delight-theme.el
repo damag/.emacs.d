8e339227cccff432b53036443c35dca03b3049ff
Damien Grassart <damien@grassart.com>
2018-11-23 22:41:13 +0100

    Add files via upload

---
+++ delight-theme.el
@@ -0,0 +1,11 @@
+(deftheme delight
+  "Created 2016-02-20.")
+
+(custom-theme-set-faces
+ 'delight
+ '(font-lock-string-face ((t (:foreground "#eb8ab7"))))
+ '(font-lock-keyword-face ((t (:foreground "skyblue3"))))
+ '(font-lock-type-face ((t (:foreground "#9773d9"))))
+ '(default ((t (:background "gray12" :foreground "#cecece")))))
+
+(provide-theme 'delight)
---
+++ init.el
@@ -0,0 +1,168 @@
+(setq custom-safe-themes '("c9809fdb29cfdcb5c0c126e4f6e0e8d23281a2e9964bdefc5776b8560f39bcc1" default))
+(load-theme 'delight)
+
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(split-window-horizontally)
+(setq split-height-threshold nil)
+(setq split-width-threshold nil)
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message nil)
+(setq column-number-mode t)
+(setq initial-major-mode 'text-mode)
+(add-hook 'text-mode-hook 'visual-line-mode)
+(add-hook 'text-mode-hook 'flyspell-mode)
+
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq make-backup-files nil)
+(server-start)
+
+(global-unset-key (kbd "C-z"))
+(global-set-key (kbd "C-x k") 'kill-this-buffer)
+(global-set-key (kbd "C-c C-f") 'ff-find-other-file)
+(global-set-key (kbd "C-c m") 'man-follow)
+(global-set-key (kbd "C-c c") 'recompile)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
+(setq compile-command "ninja -C $(git rev-parse --show-toplevel || echo .)/build")
+(setq electric-pair-mode t)
+(show-paren-mode 1)
+(setq show-paren-delay 0)
+(setq c-default-style
+      (quote
+       ((c-mode . "linux")
+	(java-mode . "java")
+	(awk-mode . "awk")
+	(other . "linux"))))
+(add-hook 'c-mode-hook '(lambda () (setq show-trailing-whitespace t)))
+(add-hook 'python-mode-hook '(lambda () (setq show-trailing-whitespace t)))
+
+(defun yank-and-indent ()
+  "Yank and then indent the newly formed region according to mode."
+  (interactive)
+  (yank)
+  (call-interactively 'indent-region))
+(global-set-key (kbd "C-y") 'yank-and-indent)
+
+(ido-mode 1)
+(ido-everywhere 1)
+(setq ido-enable-flex-matching t)
+;; disable auto searching for files unless called explicitly
+(setq ido-auto-merge-delay-time 99999)
+(define-key ido-file-dir-completion-map (kbd "C-c C-s")
+  (lambda()
+    (interactive)
+    (ido-initiate-auto-merge (current-buffer))))
+
+(which-key-mode)
+
+;; Rtags
+(require 'rtags)
+(rtags-enable-standard-keybindings)
+(define-key c-mode-base-map (kbd "M-.")
+  (function rtags-find-symbol-at-point))
+(define-key c-mode-base-map (kbd "M-,")
+  (function rtags-find-references-at-point))
+(define-key c-mode-base-map (kbd "M-[")
+  (function rtags-location-stack-back))
+(define-key c-mode-base-map (kbd "M-]")
+  (function rtags-location-stack-forward))
+
+;; Irony
+(add-hook 'after-init-hook 'global-company-mode)
+;; (eval-after-load 'company
+;;   '(add-to-list 'company-backends 'company-irony))
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends 'company-irony))
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
+
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
+
+(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(add-hook 'irony-mode-hook 'irony-eldoc)
+
+;; replace the `completion-at-point' and `complete-symbol' bindings in
+;; irony-mode's buffers by irony-mode's function
+(defun my-irony-mode-hook ()
+  (define-key irony-mode-map [remap completion-at-point]
+    'irony-completion-at-point-async)
+  (define-key irony-mode-map [remap complete-symbol]
+    'irony-completion-at-point-async))
+(add-hook 'irony-mode-hook 'my-irony-mode-hook)
+
+(setq company-idle-delay 0)
+
+;; Disaster
+(require 'disaster)
+(define-key c-mode-base-map (kbd "C-c d") 'disaster)
+
+
+;; pkgbuild
+(autoload 'pkgbuild-mode "pkgbuild-mode.el" "PKGBUILD mode." t)
+(setq auto-mode-alist (append '(("/PKGBUILD$" . pkgbuild-mode))
+			      auto-mode-alist))
+
+;; Python
+(elpy-enable)
+(setq python-shell-interpreter "ipython"
+      python-shell-interpreter-args "-i --simple-prompt")
+
+;; Web
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  ;; (setq web-mode-markup-indent-offset 2)
+  (setq-default indent-tabs-mode nil))
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+;; Dart
+(setq dart-enable-analysis-server t)
+
+;; Go
+(require 'go-eldoc)
+(add-hook 'go-mode-hook 'go-eldoc-setup)
+(add-hook 'go-mode-hook (lambda ()
+			  (set (make-local-variable 'company-backends) '(company-go))
+			  (company-mode)))
+(add-hook 'go-mode-hook (lambda ()
+			  (local-set-key (kbd "M-.") 'godef-jump)
+			  (local-set-key (kbd "C-x 4 M-.") 'godef-jump-other-window)
+			  (local-set-key (kbd "C-c C-d") 'godoc-at-point)))
+(add-hook 'before-save-hook 'gofmt-before-save)
+(eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook #'flycheck-golangci-lint-setup))
+
+
+;; Language Server Protocol
+(push 'company-lsp company-backends)
+
+;; Meson
+(add-hook 'meson-mode-hook 'company-mode)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org flycheck-irony flycheck-golangci-lint lsp-clangd lsp-css lsp-go lsp-html lsp-python lsp-typescript company-lsp go-eldoc company-go omnisharp go-mode dart-mode elpy company-tern web-mode cmake-mode disaster which-key json-mode ein pkgbuild-mode meson-mode irony-eldoc company-irony company http magit rg))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
